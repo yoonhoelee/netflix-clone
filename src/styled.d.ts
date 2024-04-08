d8eb0a914e1e24cf3850b8f50e3ad00ea2c60068
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-04-08 12:34:58 +0900

    initial commit

---
+++ src/styled.d.ts
@@ -0,0 +1,12 @@
+export interface DefaultTheme {
+    red: string;
+    black: {
+        veryDark: string;
+        darker: string;
+        lighter: string;
+    };
+    white: {
+        darker: string;
+        lighter: string;
+    };
+}
