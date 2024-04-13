acc35e16ee35e23689b6a16a4baf0d61b07dd9d9
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-04-13 19:29:37 +0900

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Routes/Header";
+
 function App() {
-  return (
-    null
-  );
+    return (
+        <Router>
+            <Header />
+            <Switch>
+                <Route path="/">
+                    <Home />
+                </Route>
+                <Route path="/tv">
+                    <Tv />
+                </Route>
+                <Route path="/search">
+                    <Search />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
