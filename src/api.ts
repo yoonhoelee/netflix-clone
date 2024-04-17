3e8cfd405698e133dab07cb32e9f9aefc779534f
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-04-17 12:08:41 +0900

    feat: use react-query to fetch movie information

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "f3a014539bf35c8269ae934395660186"
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+    return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+        (response) => response.json()
+    );
+}
