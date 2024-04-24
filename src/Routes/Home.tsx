72c3317a1c4a5bb1812b20cb91a0ae6ad9807917
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-04-24 12:35:13 +0900

    feat: added scroll value and animation

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,12 +1,12 @@
 import {useQuery} from "react-query";
 import {getMovies, IGetMoviesResult} from "../api";
 import {
-    Banner,
+    Banner, BigMovie,
     Box,
     BoxVariants,
     Info,
     InfoVariant,
-    Loader,
+    Loader, Overlay,
     Overview,
     Row,
     rowVariants,
@@ -15,7 +15,7 @@ import {
     Wrapper
 } from "../Styles";
 import {makeImagePath} from "../utils";
-import {AnimatePresence} from "framer-motion";
+import {AnimatePresence, useViewportScroll} from "framer-motion";
 import {useState} from "react";
 import {useHistory, useRouteMatch} from "react-router-dom";
 import {motion} from "framer-motion";
@@ -27,82 +27,77 @@ function Home() {
     const bigMovieMatch = useRouteMatch<{ movieId: string }>("/movies/:movieId");
     const {data, isLoading} = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
     const [index, setIndex] = useState(0);
-    const increaseIndex = () => {
-        if (data) {
-            if (leaving) return;
-            toggleLeaving();
-            const totalMovies = data?.results.length - 1;
-            const maxIndex = Math.floor(totalMovies / offset);
-            setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
-        }
-    };
     const toggleLeaving = () => setLeaving((prev) => !prev);
-    const onBoxClicked = (movieId:number) => {
+    const onBoxClicked = (movieId: number) => {
         history.push(`/movies/${movieId}`)
     }
     const [leaving, setLeaving] = useState(false);
-    return <Wrapper>
-        {isLoading ? <Loader>Loading...</Loader> :
-            <>
-                <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-                    <Title>
-                        {data?.results[0].title}
-                    </Title>
-                    <Overview>
-                        {data?.results[0].overview}
-                    </Overview>
-                </Banner>
-                <Slider>
-                    <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
-                        <Row
-                            key={index}
-                            variants={rowVariants}
-                            initial="hidden"
-                            animate="visible"
-                            exit="exit"
-                            transition={{type: "tween", duration: 1}}
-                        >
-                            {data?.results
-                                .slice(1)
-                                .slice(offset * index, offset * index + offset)
-                                .map((movie) => (
-                                    <Box
-                                        layoutId={movie.id + ""}
-                                        key={movie.id}
-                                        bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                                        variants={BoxVariants}
-                                        whileHover="hover"
-                                        initial="normal"
-                                        transition={{type: "tween"}}
-                                        onClick={() => onBoxClicked(movie.id)}
-                                    >
-                                        <Info variants={InfoVariant}>
-                                            <h4>{movie.title}</h4>
-                                        </Info>
-                                    </Box>
-                                ))}
-                        </Row>
+    const onOverlayClick = () => history.push("/");
+    const {scrollY} = useViewportScroll();
+    return (
+        <Wrapper>
+            {isLoading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Banner
+                        bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                    >
+                        <Title>{data?.results[0].title}</Title>
+                        <Overview>{data?.results[0].overview}</Overview>
+                    </Banner>
+                    <Slider>
+                        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+                            <Row
+                                variants={rowVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                transition={{ type: "tween", duration: 1 }}
+                                key={index}
+                            >
+                                {data?.results
+                                    .slice(1)
+                                    .slice(offset * index, offset * index + offset)
+                                    .map((movie) => (
+                                        <Box
+                                            layoutId={movie.id + ""}
+                                            key={movie.id}
+                                            whileHover="hover"
+                                            initial="normal"
+                                            variants={BoxVariants}
+                                            onClick={() => onBoxClicked(movie.id)}
+                                            transition={{ type: "tween" }}
+                                            bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                        >
+                                            <Info variants={InfoVariant}>
+                                                <h4>{movie.title}</h4>
+                                            </Info>
+                                        </Box>
+                                    ))}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
+                    <AnimatePresence>
+                        {bigMovieMatch ? (
+                            <>
+                                <Overlay
+                                    onClick={onOverlayClick}
+                                    exit={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                />
+                                <BigMovie
+                                    style={{ top: scrollY.get() + 100 }}
+                                    layoutId={bigMovieMatch.params.movieId}
+                                >
+                                    hello
+                                </BigMovie>
+                            </>
+                        ) : null}
                     </AnimatePresence>
-                </Slider>
-                <AnimatePresence>
-                    {bigMovieMatch ? (
-                        <motion.div
-                            layoutId={bigMovieMatch.params.movieId}
-                            style={{
-                                position: "absolute",
-                                width: "40vw",
-                                height: "80vh",
-                                backgroundColor: "red",
-                                top: 50,
-                                left: 0,
-                                right: 0,
-                                margin: "0 auto",
-                            }}
-                        />
-                    ) : null}
-                </AnimatePresence>
-            </>}
-    </Wrapper>;
+                </>
+            )}
+        </Wrapper>
+    );
 }
-
 export default Home;
--- src/Styles.ts
+++ src/Styles.ts
@@ -215,3 +215,21 @@ export const InfoVariant = {
         },
     }
 };
+
+export const Overlay = styled(motion.div)`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+`;
+
+export const BigMovie = styled(motion.div)`
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+`;
